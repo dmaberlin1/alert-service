65adeae05953a3e7501743bb4c6b82191e5545ac
dmaberlin1 <dmaberlin77@gmail.com>
2024-06-25 17:58:17 +0300

    refactoring

--- src/main/java/com/dmadev/alert/entity/Alert.java
+++ src/main/java/com/dmadev/alert/entity/Alert.java
@@ -6,11 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
